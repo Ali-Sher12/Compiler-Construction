11456be79a93111057db965ef790524e5a8defca
M.Ali Sher <sans42699@gmail.com>
2026-02-13 20:47:06 +0500

    Did the Manual Scanner

--- Compiler Construction java/src/ManualScanner.java
+++ Compiler Construction java/src/ManualScanner.java
@@ -1,3 +1,4 @@
+import javax.sound.sampled.Line;
 import java.io.*;
 import java.util.*;
 import java.nio.file.*;
@@ -10,6 +11,7 @@ public class ManualScanner
     int curr = 0;
     ArrayList<Token> Tokens_List;
     String source;
+    boolean error_found = false;
 
     ManualScanner()
     {
@@ -26,10 +28,29 @@ public class ManualScanner
         }
     }
 
+    boolean Identifier_Eligibility(){
+        if(is_lowercase(source.charAt(start))){
+            System.out.println("SyntaxError Identifier must begin with uppercase letter. "+source.substring(start,curr));
+            error_found = true;
+            return false;
+        }
+        if((curr-start) > 30)
+        {
+            System.out.println("SyntaxError Identifier length too long. "+source.substring(start,curr));
+            error_found = true;
+            return false;
+        }
+        return true;
+    }
+
     String isKEYWORD(String lexeme) {
-        if(Tokens_Dict.token_map.containsKey(lexeme))
-            return Tokens_Dict.token_map.get(lexeme);
-        return Tokens_Dict.TOK_IDENTIFIER;
+        if(Tokens_Dict.token_map.containsKey(lexeme.toLowerCase()))
+            return Tokens_Dict.token_map.get(lexeme.toLowerCase());
+        if(Identifier_Eligibility())
+            return Tokens_Dict.TOK_IDENTIFIER;
+
+        System.out.println("SyntaxError Substring neither keyword nor identifier. "+source.substring(start,curr));
+        return Tokens_Dict.NOT_A_TOKEN;
     }
 
     boolean is_digit(char num){
@@ -44,16 +65,12 @@ public class ManualScanner
         return ch;
     }
 
-    boolean match(char expected){
-        if (curr >= source.length())
-            return false;
-        if (source.charAt(curr) != expected)
-            return false;
-        curr += 1;
-        return true;
-    }
-
     void add_token(String input_type){
+        // add validations using input_type
+        if(Objects.equals(input_type, Tokens_Dict.TOK_IDENTIFIER)){
+            Identifier_Eligibility();
+        }
+        else if(Objects.equals(input_type, Tokens_Dict.NOT_A_TOKEN)) return;
         Token tempToken = new Token(input_type, source.substring(start, curr),line,start);
         Tokens_List.add(tempToken);
     }
@@ -63,36 +80,57 @@ public class ManualScanner
             return '\0';
         return source.charAt(curr);
     }
+
     char lookahead(){
         if(curr+1 >= source.length())
             return '\0';
         return source.charAt(curr+1);
     }
-    boolean is_alphanum(char ch){
-        return (('a' <= ch && ch <= 'z') ||
-                ('A' <= ch && ch <= 'Z') ||
-                ('0' <= ch && ch <= '9'));
+
+    boolean is_uppercase(char ch){
+        return ('A' <= ch && ch <= 'Z');
+    }
+
+    boolean is_lowercase(char ch){
+        return ('a' <= ch && ch <= 'z');
+    }
+
+    boolean is_alphabet(char ch){
+        return is_uppercase(ch) || is_lowercase(ch);
     }
 
     void tokenise(){
         while (curr < source.length()) {
             start = curr;
             char ch = advance();
-            if (ch == '+') add_token(Tokens_Dict.TOK_PLUS);
-            else if(ch =='-') add_token(Tokens_Dict.TOK_MINUS);
-            else if(ch =='\n')
+            if(ch =='\0') return;
+            if(ch =='\n')
                 line = line + 1;
-            else if (ch =='*') add_token(Tokens_Dict.TOK_STAR);
-            else if (ch ==' ') continue;
             else if (ch == '\t') continue;
             else if (ch == '\r') continue;
+            else if (ch ==' ') continue;
             else if (ch == '#'){
-                while (peek() != '\n'){
-                    if (peek() == '\0')
-                        return;
+                if( peek() == '#' ){
+                    while (peek() != '\n'){
+                        if (peek() == '\0')
+                            return;
+                        advance();
+                    }
+                }
+                else if( peek() == '*' ){
+                    advance();
+                    while (!(peek() == '*' && lookahead() == '#')){
+                        if (peek() == '\0') {
+                            System.out.println("SyntaxError Multi-line comments not properly closed (* missing). Line "+line);
+                            return;
+                        }
+                        advance();
+                    }
+                    advance();
                     advance();
                 }
-                advance();
+                else System.out.println("SyntaxError Single-line comments not properly closed. Line "+line);
+
             }
             else if (ch =='(') add_token(Tokens_Dict.TOK_LPAREN);
             else if (ch ==')') add_token(Tokens_Dict.TOK_RPAREN);
@@ -101,17 +139,118 @@ public class ManualScanner
             else if (ch =='[') add_token(Tokens_Dict.TOK_LSQUAR);
             else if (ch ==']') add_token(Tokens_Dict.TOK_RSQUAR);
             else if (ch ==',') add_token(Tokens_Dict.TOK_COMMA);
-            else if (ch =='^') add_token(Tokens_Dict.TOK_CARET);
-            else if (ch =='/') add_token(Tokens_Dict.TOK_SLASH);
             else if (ch ==';') add_token(Tokens_Dict.TOK_SEMICOLON);
             else if (ch =='?') add_token(Tokens_Dict.TOK_QUESTION);
             else if (ch =='%') add_token(Tokens_Dict.TOK_MOD);
+            else if (ch ==':') add_token(Tokens_Dict.TOK_COLON);
+            else if (ch =='.') add_token(Tokens_Dict.TOK_DOT);
+            else if (ch =='^') add_token(Tokens_Dict.TOK_LOGICALXOR);
             else if (ch =='=') {
-                if (peek() == '=')
+                if (peek() == '=') {
                     advance();
-                add_token(Tokens_Dict.TOK_EQEQ);
+                    add_token(Tokens_Dict.TOK_EQEQ);
+                }
+                else add_token(Tokens_Dict.TOK_EQ);
             }
-            else if (ch =='~') {
+            else if (ch =='&') {
+                if (peek() == '&') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_LOGICALAND);
+                }
+                else add_token(Tokens_Dict.TOK_AND);
+            }
+            else if (ch =='|') {
+                if (peek() == '|') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_LOGICALOR);
+                }
+                else add_token(Tokens_Dict.TOK_OR);
+            }
+            else if (ch =='*') {
+                if (peek() == '=') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_MULASSIGN);
+                }
+                else if (peek() == '*') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_EXPONENT);
+                }
+                else add_token(Tokens_Dict.TOK_STAR);
+            }
+
+            else if (ch =='/') {
+                if (peek() == '=') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_DIVASSIGN);
+                }
+                else add_token(Tokens_Dict.TOK_DIV);
+            }
+            else if (ch =='-') {
+                if (peek() == '=') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_MINUSASSIGN);
+                }
+                else if (peek() == '-') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_DEC);
+                }
+                else if (is_digit(peek())) {
+                    while (is_digit(peek()))
+                        advance();
+                    if (peek() == '.' && is_digit (lookahead())) {
+                        advance(); //consumes the '.'
+                        while (is_digit(peek())) {
+                            advance();
+                        }
+                        if(!exponent_condition_helper()){
+                            add_token(Tokens_Dict.TOK_FLOAT);
+                        }
+                    }
+                    else if (peek() == '.' && !is_digit (lookahead())) {
+                        error_found = true;
+                        System.out.println("SyntaxError Nothing after decimal point. "+line);
+                    }
+                    else if(!exponent_condition_helper())
+                    {
+                        add_token(Tokens_Dict.TOK_INTEGER);
+                    }
+                    else add_token(Tokens_Dict.TOK_MINUS);
+                }
+            }
+
+            else if (ch =='+') {
+                if (peek() == '=') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_PLUSASSIGN);
+                }
+                else if (peek() == '+') {
+                    advance();
+                    add_token(Tokens_Dict.TOK_INC);
+                }
+                else if (is_digit(peek())) {
+                    while (is_digit(peek()))
+                        advance();
+                    if (peek() == '.' && is_digit (lookahead())) {
+                        advance(); //consumes the '.'
+                        while (is_digit(peek())) {
+                            advance();
+                        }
+                        if(!exponent_condition_helper()){
+                            add_token(Tokens_Dict.TOK_FLOAT);
+                        }
+                    }
+                    else if (peek() == '.' && !is_digit (lookahead())) {
+                        System.out.println("SyntaxError Nothing after decimal point. "+line);
+                        error_found = true;
+                    }
+                    else if(!exponent_condition_helper())
+                    {
+                        add_token(Tokens_Dict.TOK_INTEGER);
+                    }
+                }
+                else add_token(Tokens_Dict.TOK_PLUS);
+            }
+            else if (ch =='!') {
                 if (peek() == '=') {
                     advance();
                     add_token(Tokens_Dict.TOK_NE);
@@ -134,54 +273,16 @@ public class ManualScanner
                 }
                 else add_token(Tokens_Dict.TOK_GT);
             }
-            else if (ch ==':' ) {
-                if (peek() == '=') {
-                    advance();
-                    add_token(Tokens_Dict.TOK_ASSIGN);
-                }
-                else add_token(Tokens_Dict.TOK_COLON);
-            }
-            else if (ch =='.') {
-                if (!is_digit(peek()))
-                    add_token(Tokens_Dict.TOK_DOT);
-                else{
-                    while (true){
-                        if(is_digit(peek()))
-                            advance();
-                        else {
-                            add_token(Tokens_Dict.TOK_FLOAT);
-                            break;
-                        }
-                    }
-                }
-            }
-
-            else if (is_digit(ch)) {
-                while (is_digit(peek()))
-                    advance();
-                if (peek() == '.' && is_digit (lookahead())) {
-                    advance(); //consumes the '.'
-                    while (is_digit(peek())) {
-                        advance();
-                    }
-                    add_token(Tokens_Dict.TOK_FLOAT);
-                }
-                else
-                    add_token(Tokens_Dict.TOK_INTEGER);
-            }
 
             else if (ch =='\'') {
-                while (peek() != '\'') {
-                    if (peek() == '\0')
-                        break;
+                advance();
+                if (peek() != '\''){
+                    System.out.println("Syntax error Invalid char at line  "+ line);
+                    error_found = true;                }
+                else{
                     advance();
+                    add_token(Tokens_Dict.TOK_CHAR);
                 }
-                if (peek() == '\'') {
-                    advance();
-                    add_token(Tokens_Dict.TOK_STRING);
-                }
-                else
-                    System.out.println("Syntax error  Unclosed string at line  "+ line);
             }
 
             else if (ch =='\"') {
@@ -194,12 +295,33 @@ public class ManualScanner
                     advance();
                     add_token(Tokens_Dict.TOK_STRING);
                 }
-                else
+                else{
                     System.out.println("Syntax error  Unclosed string at line  "+ line);
+                    error_found = true;                }
             }
 
-            else if (is_alphanum (ch)) {
-                while (is_alphanum(peek()) || peek() == '_') {
+            else if (is_digit(ch)) {
+                while (is_digit(peek()))
+                    advance();
+                if (peek() == '.' && is_digit (lookahead())) {
+                    advance(); //consumes the '.'
+                    while (is_digit(peek())) {
+                        advance();
+                    }
+                    if(!exponent_condition_helper()){
+                        add_token(Tokens_Dict.TOK_FLOAT);
+                    }
+                }
+                else if (peek() == '.' && !is_digit (lookahead())) {
+                    System.out.println("SyntaxError Nothing after decimal point. "+line);
+                    error_found = true;
+                }
+                else if(!exponent_condition_helper()) {
+                    add_token(Tokens_Dict.TOK_INTEGER);
+                }
+            }
+            else if (is_alphabet (ch)) {
+                while (is_alphabet(peek()) || peek() == '_' || is_digit(peek())) {
                     if (peek() == '\0')
                         break;
                     advance ();
@@ -208,7 +330,34 @@ public class ManualScanner
                 add_token(isKEYWORD(token_lexeme));
             }
         }
-        return Tokens_List;
+    }
+
+    private boolean exponent_condition_helper()
+    {
+        if (peek() == 'e' || peek() == 'E'){
+            advance();
+            if (peek() == '+' || peek() == '-') {
+                advance();
+                if (!is_digit(peek())) {
+                    System.out.println("SyntaxError Exponent is invalid. "+line);
+                    error_found = true;
+                }
+                while (is_digit(peek()))
+                    advance();
+                if(!error_found) add_token(Tokens_Dict.TOK_FLOAT);
+            }
+            else if (is_digit (peek())){
+                while (is_digit(peek()))
+                    advance();
+                add_token(Tokens_Dict.TOK_FLOAT);                        }
+
+            else{
+                System.out.println("SyntaxError Exponent is invalid. "+line);
+                error_found = true;
+            }
+            return true;
+        }
+        return false;
     }
 
     void printList()
--- Compiler Construction java/src/TokenType.java
+++ Compiler Construction java/src/TokenType.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 
 public class TokenType
 {
+    String NOT_A_TOKEN    = "NOT_A_TOKEN";
     String TOK_LPAREN     = "TOK_LPAREN";     //  (
     String TOK_RPAREN     = "TOK_RPAREN";     //  )
     String TOK_LCURLY     = "TOK_LCURLY";     //  {
@@ -10,49 +11,56 @@ public class TokenType
     String TOK_RSQUAR     = "TOK_RSQUAR";     //  ]
     String TOK_COMMA      = "TOK_COMMA" ;     //  ,
     String TOK_DOT        = "TOK_DOT"   ;     //  .
+    // I'm keeping plus and minus because identifiers can also be added or subtracted.
     String TOK_PLUS       = "TOK_PLUS"  ;     //  +
     String TOK_MINUS      = "TOK_MINUS" ;     //  -
     String TOK_STAR       = "TOK_STAR"  ;     //  *
-    String TOK_SLASH      = "TOK_SLASH" ;     //  /
-    String TOK_CARET      = "TOK_CARET" ;     //  ^
+    String TOK_EXPONENT   = "TOK_EXPONENT";   //  **
+    String TOK_DIV        = "TOK_DIV" ;       //  /
     String TOK_MOD        = "TOK_MOD"   ;     //  %
     String TOK_COLON      = "TOK_COLON" ;     //  :
     String TOK_SEMICOLON  = "TOK_SEMICOLON";  //  ;
     String TOK_QUESTION   = "TOK_QUESTION";   //  ?
-    String TOK_NOT        = "TOK_NOT";        //  ~
+    String TOK_NOT        = "TOK_NOT";        //  !
     String TOK_GT         = "TOK_GT" ;        //  >
     String TOK_LT         = "TOK_LT" ;        //  <
     String TOK_EQ         = "TOK_EQ" ;        //  =
+    String TOK_LOGICALAND = "TOK_LOGICALAND"; //  &
+    String TOK_LOGICALOR  = "TOK_LOGICALOR" ; //  |
+    String TOK_LOGICALXOR = "TOK_LOGICALXOR"; //  ^
     // Two-char tokens
     String TOK_GE         = "TOK_GE"    ;     //  >=
     String TOK_LE         = "TOK_LE"    ;     //  <=
-    String TOK_NE         = "TOK_NE"    ;     //  ~=
+    String TOK_NE         = "TOK_NE"    ;     //  !=
     String TOK_EQEQ       = "TOK_EQEQ"  ;     //  ==
-    String TOK_ASSIGN     = "TOK_ASSIGN";     //  :=
-    String TOK_GTGT       = "TOK_GTGT"  ;     //  >>
-    String TOK_LTLT       = "TOK_LTLT"  ;     //  <<
+    String TOK_AND        = "TOK_ASSIGN";     //  &&
+    String TOK_OR         = "TOK_ASSIGN";     //  ||
+    String TOK_INC        = "TOK_ASSIGN";     //  ++
+    String TOK_DEC        = "TOK_ASSIGN";     //  --
+    String TOK_PLUSASSIGN   = "TOK_ASSIGN";     //  +=
+    String TOK_MINUSASSIGN  = "TOK_ASSIGN";     //  -=
+    String TOK_MULASSIGN  = "TOK_ASSIGN";     //  *=
+    String TOK_DIVASSIGN  = "TOK_ASSIGN";     //  /=
     // Literals
     String TOK_IDENTIFIER = "TOK_IDENTIFIER";
     String TOK_STRING     = "TOK_STRING";
+    String TOK_CHAR     = "TOK_CHAR";
     String TOK_INTEGER    = "TOK_INTEGER";
     String TOK_FLOAT      = "TOK_FLOAT";
     // Keywords
-    String TOK_IF         = "TOK_IF";
-    String TOK_THEN       = "TOK_THEN";
-    String TOK_ELSE       = "TOK_ELSE";
+    String TOK_CONDITION         = "TOK_CONDITION";
+    String TOK_START       = "TOK_START";
+    String TOK_FINISH       = "TOK_FINISH";
     String TOK_TRUE       = "TOK_TRUE";
     String TOK_FALSE      = "TOK_FALSE";
-    String TOK_AND        = "TOK_AND";
-    String TOK_OR         = "TOK_OR";
-    String TOK_WHILE      = "TOK_WHILE";
-    String TOK_DO         = "TOK_DO";
-    String TOK_FOR        = "TOK_FOR";
-    String TOK_FUNC       = "TOK_FUNC";
-    String TOK_NULL       = "TOK_NULL";
-    String TOK_END        = "TOK_END";
-    String TOK_PRINT      = "TOK_PRINT";
-    String TOK_PRINTLN    = "TOK_PRINTLN";
-    String TOK_RET        = "TOK_RET";
+    String TOK_LOOP        = "TOK_LOOP";
+    String TOK_DECLARE         = "TOK_DECLARE";
+    String TOK_OUTPUT      = "TOK_OUTPUT";
+    String TOK_INPUT         = "TOK_INPUT";
+    String TOK_FUNCTION        = "TOK_FUNCTION";
+    String TOK_RETURN       = "TOK_RETURN";
+    String TOK_BREAK       = "TOK_BREAK";
+    String TOK_CONTINUE        = "TOK_CONTINUE";
 
     /////////////////////////////////////////
     HashMap<String, String> token_map;
@@ -60,23 +68,19 @@ public class TokenType
     public TokenType()
     {
         token_map = new HashMap<String,String>();
-        token_map.put("if"     ,TOK_IF);
-        token_map.put("else"   ,TOK_ELSE);
-        token_map.put("then"   ,TOK_THEN);
+        token_map.put("condition"   ,TOK_CONDITION);
+        token_map.put("start"  ,TOK_START);
+        token_map.put("finish"   ,TOK_FINISH);
         token_map.put("true"   ,TOK_TRUE);
         token_map.put("false"  ,TOK_FALSE);
-        token_map.put("and"    ,TOK_AND);
-        token_map.put("or"     ,TOK_OR);
-        token_map.put("while"  ,TOK_WHILE);
-        token_map.put("do"     ,TOK_DO);
-        token_map.put("for"    ,TOK_FOR);
-        token_map.put("func"   ,TOK_FUNC);
-        token_map.put("null"   ,TOK_NULL);
-        token_map.put("end"    ,TOK_END);
-        token_map.put("print"  ,TOK_PRINT);
-        token_map.put("println",TOK_PRINTLN);
-        token_map.put("ret"    ,TOK_RET);
-        System.out.println(token_map.get("apple")); // 5
+        token_map.put("loop"   ,TOK_LOOP);
+        token_map.put("declare"  ,TOK_DECLARE);
+        token_map.put("output"   ,TOK_OUTPUT);
+        token_map.put("input"  ,TOK_INPUT);
+        token_map.put("function"   ,TOK_FUNCTION);
+        token_map.put("return"  ,TOK_RETURN);
+        token_map.put("break"   ,TOK_BREAK);
+        token_map.put("continue"  ,TOK_CONTINUE);
     }
 
 }
