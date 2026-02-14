842979df8ab1c64753410c3732d17656fa9f5140
M.Ali Sher <sans42699@gmail.com>
2026-02-14 23:24:01 +0500

    Basic Implementation via JFLEX

--- Compiler Construction java/src/Main.java
+++ Compiler Construction java/src/Main.java
@@ -3,10 +3,10 @@ import java.util.*;
 import java.nio.file.*;
 
 class Main{
-    static void main()
+    public static void main(String[] args)
     {
         try {
-            PrintStream fileOut = new PrintStream("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/TestResults.txt");
+            PrintStream fileOut = new PrintStream("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/TestResultsManual.txt");
             System.setOut(fileOut);
             System.out.println("\n\n\t\t <<<<<<<<<<<<<<<<<  Test1 >>>>>>>>>>>>>>>>:\n");
             ManualScanner ms1 = new ManualScanner("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/test1.lang");
@@ -32,6 +32,48 @@ class Main{
             e.printStackTrace();
         }
 
+        // Scanner must read files into "/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/TestResultsJFLEX.txt"
 
+        try {
+            PrintStream fileOut = new PrintStream("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/TestResultsJFLEX.txt");
+            System.setOut(fileOut);
+
+            // Test 1
+            System.out.println("\n\n\t\t <<<<<<<<<<<<<<<<<  Test1 >>>>>>>>>>>>>>>>:\n");
+            scanFile("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/test1.lang");
+
+            // Test 2
+            System.out.println("\n\n\t\t <<<<<<<<<<<<<<<<<  Test2 >>>>>>>>>>>>>>>>:\n");
+            scanFile("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/test2.lang");
+
+            // Test 3
+            System.out.println("\n\n\t\t <<<<<<<<<<<<<<<<<  Test3 >>>>>>>>>>>>>>>>:\n");
+            scanFile("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/test3.lang");
+
+            // Test 4
+            System.out.println("\n\n\t\t <<<<<<<<<<<<<<<<<  Test4 >>>>>>>>>>>>>>>>:\n");
+            scanFile("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/test4.lang");
+
+            // Test 5
+            System.out.println("\n\n\t\t <<<<<<<<<<<<<<<<<  Test5 >>>>>>>>>>>>>>>>:\n");
+            scanFile("/home/jarvis2026/Documents/Repos/compiler construction/Compiler Construction java/tests/test5.lang");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void scanFile(String filename) {
+        try {
+            Scanner scanner = new Scanner(new FileReader(filename));
+            Scanner.Token token;
+
+            while ((token = scanner.yylex()) != null) {
+                System.out.println(token);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading file " + filename + ": " + e.getMessage());
+        }
     }
 }
